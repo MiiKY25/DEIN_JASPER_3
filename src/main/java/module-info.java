7062aa44f7a93fd10d8a364f8c1e2f9ecf4b3f6b
MiiKY <mikel.ra.2003@gmail.com>
2025-01-05 00:00:59 +0100

    add:estructura creada

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module org.mikel.dein_jasper_3 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires net.sf.jasperreports.core;
 
-
+    opens org.mikel.dein_jasper_3.controladores to javafx.fxml;
     opens org.mikel.dein_jasper_3 to javafx.fxml;
     exports org.mikel.dein_jasper_3;
 }
