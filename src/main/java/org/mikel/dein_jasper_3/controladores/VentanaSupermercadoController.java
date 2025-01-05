6dec0bfcb51e9c31c5979b5943d43c6a99c04ca2
MiiKY <mikel.ra.2003@gmail.com>
2025-01-05 01:01:23 +0100

    add:primer informe creado

--- src/main/java/org/mikel/dein_jasper_3/controladores/VentanaSupermercadoController.java
+++ src/main/java/org/mikel/dein_jasper_3/controladores/VentanaSupermercadoController.java
@@ -2,17 +2,61 @@ package org.mikel.dein_jasper_3.controladores;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.view.JasperViewer;
+import org.mikel.dein_jasper_3.bbdd.ConexionBBDD;
+
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class VentanaSupermercadoController {
 
     @FXML
-    void accionGraficos(ActionEvent event) {
-
-    }
-
-    @FXML
     void accionProductos(ActionEvent event) {
+        ConexionBBDD db;
+        try {
+            // Crear una nueva conexión a la base de datos
+            db = new ConexionBBDD();
 
+            // Cargar el archivo Jasper del informe
+            InputStream reportStream = db.getClass().getResourceAsStream("/jasper/Productos.jasper");
+
+            // Verificar si el archivo fue encontrado
+            if (reportStream == null) {
+                System.out.println("Archivo NO encontrado");
+                return;
+            }
+
+            // Cargar el informe Jasper
+            JasperReport report = (JasperReport) JRLoader.loadObject(reportStream);
+
+            // Parámetros del informe
+            Map<String, Object> parameters = new HashMap<>();
+
+            // Añadir la ruta de las imágenes
+            String imagePath = db.getClass().getResource("/imagenes/").toString(); // Ruta de la carpeta de imágenes
+            parameters.put("IMAGE_PATH", imagePath);
+
+            // Llenar el informe con datos
+            JasperPrint jprint = JasperFillManager.fillReport(report, parameters, db.getConnection());
+
+            // Mostrar el informe
+            JasperViewer viewer = new JasperViewer(jprint, false);
+            viewer.setVisible(true);
+
+        } catch (SQLException e) {
+            System.err.println("Error de conexión a la base de datos:");
+            e.printStackTrace();
+        } catch (JRException e) {
+            System.err.println("Error al procesar el informe Jasper:");
+            e.printStackTrace();
+        }
     }
 
     @FXML
@@ -22,6 +66,11 @@ public class VentanaSupermercadoController {
 
     @FXML
     void accionTabla(ActionEvent event) {
+
+    }
+
+    @FXML
+    void accionGraficos(ActionEvent event) {
 
     }
 
